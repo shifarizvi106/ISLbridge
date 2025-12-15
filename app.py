7e309d2c2a6a4a58b755b43ce160d6cf3a661955
Shifa Rizvi <shifasayed106@gmail.com>
2025-12-15 20:24:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+def text_to_signs(text):
+    words = clean_and_split(text)
+    words = remove_stopwords(words)
+
+    signs = []
+    for word in words:
+        if word in SIGN_DICT:
+            signs.append(SIGN_DICT[word])
+
+    return signs
 
