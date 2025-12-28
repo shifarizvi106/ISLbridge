1a137a15c3d6bc9faab22d97bc15798ccf3b3dfb
Shifa Rizvi <shifasayed106@gmail.com>
2025-12-28 20:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import json
-
-def text_to_signs(text):
-    words = clean_and_split(text)
-    words = remove_stopwords(words)
-
-    signs = []
-    for word in words:
-        if word in SIGN_DICT:
-          signs.append(SIGN_DICT[word]["file"])
-
-    return signs
 
