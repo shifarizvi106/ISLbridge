3fad0ae9d1e3570d36ea50a79209892fc1172a48
Shifa Rizvi <shifasayed106@gmail.com>
2026-01-17 12:06:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    data = request.get_json()
+    text = data.get("text", "")
+    return jsonify({"text": text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
